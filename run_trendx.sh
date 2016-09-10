0d6e9e94075d520a82340ab0b01f4b3e6d39e556
EC2 Default User <ec2-user@ip-172-16-0-100.ec2.internal>
2016-09-10 02:11:51 +0000

    keep logs

--- run_trendx.sh
+++ run_trendx.sh
@@ -4,14 +4,14 @@
 CONFIG_FILE=$1
 NAME=$2
 BUCKET=$3
-
+#data=s3://${BUCKET}/run3/1layer/ATSE1036_BASIC_ALL_SVMF_0812G/#dtrain.cache\
 # submit the job to YARN
 # 20g * 4 * 2 < M-30
 time ../../dmlc-core/tracker/dmlc-submit --cluster=yarn --num-workers=10 --worker-cores=2 --server-memory=20g --worker-memory=20g\
     ../../xgboost ${CONFIG_FILE} nthread=4\
     data=s3://${BUCKET}/run3/1layer/ATSE1036_BASIC_ALL_SVMF_0812G/#dtrain.cache\
     eval[vmalicioustrain]=s3://${BUCKET}/run3/data/run2verticnormal/#test.cache1\
-    eval[vnomraltrain]=s3://${BUCKET}/run3/data/run2verticmalware/#test.cache1
+    eval[vnomraltrain]=s3://${BUCKET}/run3/data/run2verticmalware/#test.cache1\
     model_dir=s3://${BUCKET}/run3/test1/${NAME}/  2>&1 | tee ${NAME}.log
 
 cat ${CONFIG_FILE} >>  ${NAME}.log
